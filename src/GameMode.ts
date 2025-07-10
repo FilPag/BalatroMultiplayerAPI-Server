c5615a2aec58a31a5eb991728a5143f95288f1a7
FilPag <pagliarofilip@gmail.com>
2025-07-10 23:34:58 +0200

    coop v.0.1

--- src/GameMode.ts
+++ src/GameMode.ts
@@ -1,43 +1,137 @@
 import type { GameMode } from "./actions.js";
+import { LobbyOptions } from "./Lobby.js";
 
 type GameModeData = {
-	startingLives: number;
 
-	getBlindFromAnte: (
-		ante: number,
-		options: any,
-	) => {
-		small?: string;
-		big?: string;
-		boss?: string;
-	};
+  defaultOptions: LobbyOptions
 
-	// TODO: Validate lobby options when they differ per gamemode
+  getBlindFromAnte: (
+    ante: number,
+    options: any,
+  ) => {
+    small?: string;
+    big?: string;
+    boss?: string;
+  };
+
+  // TODO: Validate lobby options when they differ per gamemode
 };
 
 const GameModes: {
-	[key in GameMode]: GameModeData;
+  [key in GameMode]: GameModeData;
 } = {
-	attrition: {
-		startingLives: 4,
-		getBlindFromAnte: (ante, options) => {
-			return { boss: "bl_pvp" };
-		},
-	},
-	showdown: {
-		startingLives: 2,
-		getBlindFromAnte: (ante, options) => {
-			const starting_antes = options?.showdown_starting_antes ? parseInt(options.showdown_starting_antes) : 3
-      if (ante <= starting_antes) return { }
-      else return { small: 'bl_pvp', big: 'bl_pvp', boss: 'bl_pvp' }
-		},
-	},
-	survival: {
-		startingLives: 1,
-		getBlindFromAnte: (ante, options) => {
-			return { };
-		},
-	}
+  attrition: {
+    defaultOptions: {
+      back: "Red Deck",
+      challenge: 0,
+      custom_seed: "random",
+      death_on_round_loss: false,
+      different_decks: false,
+      different_seeds: false,
+      disable_live_and_timer_hud: false,
+      gamemode: "gamemode_mp_attrition",
+      gold_on_life_loss: true,
+      multiplayer_jokers: true,
+      no_gold_on_round_loss: false,
+      normal_bosses: false,
+      pvp_start_round: 2,
+      ruleset: "ruleset_mp_standard",
+      showdown_starting_antes: 3,
+      sleeve: "sleeve_casl_none",
+      stake: 1,
+      starting_lives: 4,
+      timer_base_seconds: 150,
+      timer_increment_seconds: 60,
+    },
+    getBlindFromAnte: (ante, options) => {
+      return { boss: "bl_pvp" };
+    },
+  },
+  showdown: {
+    defaultOptions: {
+      back: "Red Deck",
+      challenge: 0,
+      custom_seed: "random",
+      death_on_round_loss: false,
+      different_decks: false,
+      different_seeds: false,
+      disable_live_and_timer_hud: false,
+      gamemode: "gamemode_mp_showdown",
+      gold_on_life_loss: true,
+      multiplayer_jokers: true,
+      no_gold_on_round_loss: false,
+      normal_bosses: false,
+      pvp_start_round: 2,
+      ruleset: "ruleset_mp_standard",
+      showdown_starting_antes: 3,
+      sleeve: "sleeve_casl_none",
+      stake: 1,
+      starting_lives: 4,
+      timer_base_seconds: 150,
+      timer_increment_seconds: 60,
+    },
+    getBlindFromAnte: (ante, options) => {
+      const starting_antes = options?.showdown_starting_antes
+        ? parseInt(options.showdown_starting_antes)
+        : 3;
+      if (ante <= starting_antes) return {};
+      else return { small: "bl_pvp", big: "bl_pvp", boss: "bl_pvp" };
+    },
+  },
+  survival: {
+    defaultOptions: {
+      back: "Red Deck",
+      challenge: 0,
+      custom_seed: "random",
+      death_on_round_loss: false,
+      different_decks: false,
+      different_seeds: false,
+      disable_live_and_timer_hud: false,
+      gamemode: "gamemode_mp_survival",
+      gold_on_life_loss: true,
+      multiplayer_jokers: true,
+      no_gold_on_round_loss: false,
+      normal_bosses: false,
+      pvp_start_round: 20,
+      ruleset: "ruleset_mp_standard",
+      showdown_starting_antes: 3,
+      sleeve: "sleeve_casl_none",
+      stake: 1,
+      starting_lives: 4,
+      timer_base_seconds: 150,
+      timer_increment_seconds: 60,
+    },
+    getBlindFromAnte: (ante, options) => {
+      return {};
+    },
+  },
+  coopSurvival: {
+    defaultOptions: {
+      back: "Red Deck",
+      challenge: 0,
+      custom_seed: "random",
+      death_on_round_loss: false,
+      different_decks: true,
+      different_seeds: true,
+      disable_live_and_timer_hud: false,
+      gamemode: "gamemode_mp_coopSurvival",
+      ruleset: "ruleset_mp_coop",
+      gold_on_life_loss: true,
+      multiplayer_jokers: false,
+      no_gold_on_round_loss: false,
+      normal_bosses: true,
+      pvp_start_round: 2,
+      showdown_starting_antes: 3,
+      sleeve: "sleeve_casl_none",
+      stake: 1,
+      starting_lives: 2,
+      timer_base_seconds: 150,
+      timer_increment_seconds: 60,
+    },
+    getBlindFromAnte: (ante, options) => {
+      return {};
+    },
+  },
 };
 
 export default GameModes;
--- src/actions.ts
+++ src/actions.ts
@@ -1,24 +1,25 @@
+import { GameStateData, LobbyData } from "./Client.js";
+import { LobbyOptions } from "./Lobby.js";
+
 // Server to Client
+type StateActionData = Partial<Omit<GameStateData, 'score' | 'highest_score' > & { score: string; highest_score: string}>
 export type ActionConnected = { action: 'connected' }
 export type ActionError = { action: 'error'; message: string }
-export type ActionJoinedLobby = { action: 'joinedLobby'; code: string; type: GameMode }
+export type ActionJoinedLobby = { action: 'joinedLobby'; code: string; }
 export type ActionLobbyInfo = {
 	action: 'lobbyInfo'
 	host: string
-	hostHash: string
-	hostCached: boolean
-	guest?: string
-	guestHash?: string
-	guestCached?: boolean
-	guestReady?: boolean
 	isHost: boolean
+	players?: Array<LobbyData & { id: string }>
+	local_id?: string
 }
 export type ActionStopGame = { action: 'stopGame' }
 export type ActionStartGame = {
-	action: 'startGame'
-	deck: string
-	stake?: number
-	seed?: string
+  action: 'startGame'
+  deck: string
+  stake?: number
+  seed?: string
+  players: Array<StateActionData & {id: string}>
 }
 export type ActionStartBlind = { action: 'startBlind' }
 export type ActionWinGame = { action: 'winGame' }
@@ -29,18 +30,17 @@ export type ActionGameInfo = {
 	big?: string
 	boss?: string
 }
-export type ActionPlayerInfo = { action: 'playerInfo'; lives: number }
-export type ActionEnemyInfo = {
-	action: 'enemyInfo'
-	score: string
-	handsLeft: number
-	skips: number
-	lives: number
+
+// Generic game state update action - can update any combination of game state fields
+export type ActionGameStateUpdate = { 
+  action: 'gameStateUpdate'
+  id: string
+  updates: StateActionData
 }
 export type ActionEndPvP = { action: 'endPvP'; lost: boolean }
-export type ActionLobbyOptions = { action: 'lobbyOptions', gamemode: string }
+export type ActionLobbyOptions = { action: 'lobbyOptions', options: LobbyOptions}
 export type ActionRequestVersion = { action: 'version' }
-export type ActionEnemyLocation = { action: 'enemyLocation'; location: string }
+export type ActionEnemyLocation = { action: 'enemyLocation'; playerId: string; location: string }
 export type ActionSendPhantom = { action: 'sendPhantom', key: string }
 export type ActionRemovePhantom = { action: 'removePhantom', key: string }
 export type ActionSpeedrun = { action: 'speedrun' }
@@ -59,6 +59,7 @@ export type ActionGetNemesisStatsRequest = { action: 'endGameStatsRequested' }
 export type ActionReceiveNemesisStatsRequest = { action: 'nemesisEndGameStats', reroll_count: string, reroll_cost_total:string, vouchers:string }
 export type ActionStartAnteTimer = { action: 'startAnteTimer', time: number }
 export type ActionPauseAnteTimer = { action: 'pauseAnteTimer', time: number }
+export type ActionSetBossBlind = { action: 'setBossBlind'; bossKey: string; targetScore?: string};
 export type ActionServerToClient =
 	| ActionConnected
 	| ActionError
@@ -70,8 +71,7 @@ export type ActionServerToClient =
 	| ActionWinGame
 	| ActionLoseGame
 	| ActionGameInfo
-	| ActionPlayerInfo
-	| ActionEnemyInfo
+	| ActionGameStateUpdate
 	| ActionEndPvP
 	| ActionLobbyOptions
 	| ActionRequestVersion
@@ -95,6 +95,8 @@ export type ActionServerToClient =
 	| ActionReceiveNemesisStatsRequest
 	| ActionStartAnteTimer
 	| ActionPauseAnteTimer
+	| ActionSetBossBlind
+
 // Client to Server
 export type ActionUsername = { action: 'username'; username: string; modHash: string }
 export type ActionCreateLobby = { action: 'createLobby'; gameMode: GameMode }
@@ -108,108 +110,134 @@ export type ActionStartGameRequest = { action: 'startGame' }
 export type ActionReadyBlind = { action: 'readyBlind' }
 export type ActionUnreadyBlind = { action: 'unreadyBlind' }
 export type ActionPlayHand = {
-	action: 'playHand'
-	score: string
-	handsLeft: number
-	hasSpeedrun: boolean
-}
-export type ActionGameInfoRequest = { action: 'gameInfo' }
-export type ActionPlayerInfoRequest = { action: 'playerInfo' }
-export type ActionEnemyInfoRequest = { action: 'enemyInfo' }
-export type ActionFailRound = { action: 'failRound' }
+  action: "playHand";
+  score: string;
+  hands_left: number;
+  target_score?: string; // For coop survival mode
+  hasSpeedrun: boolean;
+};
+export type ActionGameInfoRequest = { action: "gameInfo" };
+export type ActionPlayerInfoRequest = { action: "playerInfo" };
+export type ActionEnemyInfoRequest = { action: "enemyInfo" };
+export type ActionFailRound = { action: "failRound" };
 export type ActionSetAnte = {
-	action: 'setAnte'
-	ante: number
-}
-export type ActionVersion = { action: 'version'; version: string }
-export type ActionSetLocation = { action: 'setLocation'; location: string }
-export type ActionNewRound = { action: 'newRound' }
-export type ActionSetFurthestBlind = { action: 'setFurthestBlind', furthestBlind: number}
-export type ActionSkip = { action: 'skip', skips: number }
-export type ActionLetsGoGamblingNemesisRequest = { action: 'letsGoGamblingNemesis' }
-export type ActionEatPizzaRequest = { action: 'eatPizza', whole: boolean }
-export type ActionSoldJokerRequest = { action: 'soldJoker' }
-export type ActionSpentLastShopRequest = { action: 'spentLastShop', amount: number }
-export type ActionMagnetRequest = { action: 'magnet' }
-export type ActionMagnetResponseRequest = { action: 'magnetResponse', key: string }
-export type ActionGetEndGameJokersResponse = { action: 'getEndGameJokers' }
-export type ActionReceiveEndGameJokersResponse = { action: 'receiveEndGameJokers', keys: string }
-export type ActionGetNemesisDeckResponse = { action: 'getNemesisDeck' }
-export type ActionReceiveNemesisDeckResponse = { action: 'receiveNemesisDeck', cards: string }
+  action: "setAnte";
+  ante: number;
+};
+export type ActionVersion = { action: "version"; version: string };
+export type ActionSetLocation = { action: "setLocation"; location: string };
+export type ActionNewRound = { action: "newRound" };
+export type ActionSetFurthestBlind = {
+  action: "setFurthestBlind";
+  furthest_blind: number;
+};
+export type ActionSkip = { action: "skip"; skips: number };
+export type ActionLetsGoGamblingNemesisRequest = {
+  action: "letsGoGamblingNemesis";
+};
+export type ActionEatPizzaRequest = { action: "eatPizza"; whole: boolean };
+export type ActionSoldJokerRequest = { action: "soldJoker" };
+export type ActionSpentLastShopRequest = {
+  action: "spentLastShop";
+  amount: number;
+};
+export type ActionMagnetRequest = { action: "magnet" };
+export type ActionMagnetResponseRequest = {
+  action: "magnetResponse";
+  key: string;
+};
+export type ActionGetEndGameJokersResponse = { action: "getEndGameJokers" };
+export type ActionReceiveEndGameJokersResponse = {
+  action: "receiveEndGameJokers";
+  keys: string;
+};
+export type ActionGetNemesisDeckResponse = { action: "getNemesisDeck" };
+export type ActionReceiveNemesisDeckResponse = {
+  action: "receiveNemesisDeck";
+  cards: string;
+};
+export type ActionStartAnteTimerRequest = {
+  action: "startAnteTimer";
+  time: number;
+};
+export type ActionPauseAnteTimerRequest = {
+  action: "pauseAnteTimer";
+  time: number;
+};
 export type ActionGetNemesisStatsResponse = { action: 'endGameStatsRequested' }
 export type ActionReceiveNemesisStatsResponse = { action: 'nemesisEndGameStats', reroll_count: string,reroll_cost_total:string, vouchers:string }
-export type ActionStartAnteTimerRequest = { action: 'startAnteTimer', time: number }
-export type ActionPauseAnteTimerRequest = { action: 'pauseAnteTimer', time: number }
-export type ActionFailTimer = { action: 'failTimer' }
-export type ActionSyncClient = { action: 'syncClient', isCached: boolean }
+export type ActionFailTimer = { action: "failTimer" };
+export type ActionSyncClient = { action: "syncClient"; isCached: boolean };
 export type ActionClientToServer =
-	| ActionUsername
-	| ActionCreateLobby
-	| ActionJoinLobby
-	| ActionLeaveLobby
-	| ActionReadyLobby
-	| ActionUnreadyLobby
-	| ActionLobbyInfoRequest
-	| ActionStopGameRequest
-	| ActionStartGameRequest
-	| ActionReadyBlind
-	| ActionPlayHand
-	| ActionGameInfoRequest
-	| ActionPlayerInfoRequest
-	| ActionEnemyInfoRequest
-	| ActionUnreadyBlind
-	| ActionLobbyOptions
-	| ActionFailRound
-	| ActionSetAnte
-	| ActionVersion
-	| ActionSetLocation
-	| ActionNewRound
-	| ActionSetFurthestBlind
-	| ActionSkip
-	| ActionSendPhantom
-	| ActionRemovePhantom
-	| ActionAsteroid
-	| ActionLetsGoGamblingNemesisRequest
-	| ActionEatPizzaRequest
-	| ActionSoldJokerRequest
-	| ActionSpentLastShopRequest
-	| ActionMagnetRequest
-	| ActionMagnetResponseRequest
-	| ActionGetEndGameJokersResponse
-	| ActionReceiveEndGameJokersResponse
-	| ActionGetNemesisDeckResponse
-	| ActionReceiveNemesisDeckResponse
+  | ActionUsername
+  | ActionCreateLobby
+  | ActionJoinLobby
+  | ActionLeaveLobby
+  | ActionLobbyInfoRequest
+  | ActionStopGameRequest
+  | ActionStartGameRequest
+  | ActionReadyBlind
+  | ActionPlayHand
+  | ActionGameInfoRequest
+  | ActionPlayerInfoRequest
+  | ActionEnemyInfoRequest
+  | ActionUnreadyBlind
+  | ActionLobbyOptions
+  | ActionFailRound
+  | ActionSetAnte
+  | ActionVersion
+  | ActionSetLocation
+  | ActionNewRound
+  | ActionSetFurthestBlind
+  | ActionSkip
+  | ActionSendPhantom
+  | ActionRemovePhantom
+  | ActionAsteroid
+  | ActionLetsGoGamblingNemesisRequest
+  | ActionEatPizzaRequest
+  | ActionSoldJokerRequest
+  | ActionSpentLastShopRequest
+  | ActionMagnetRequest
+  | ActionMagnetResponseRequest
+  | ActionGetEndGameJokersResponse
+  | ActionReceiveEndGameJokersResponse
+  | ActionGetNemesisDeckResponse
+  | ActionReceiveNemesisDeckResponse
 	| ActionGetNemesisStatsResponse
 	| ActionReceiveNemesisStatsResponse
-	| ActionStartAnteTimerRequest
-	| ActionPauseAnteTimerRequest
-	| ActionFailTimer
-	| ActionSyncClient
+  | ActionStartAnteTimerRequest
+  | ActionPauseAnteTimerRequest
+  | ActionFailTimer
+  | ActionSyncClient
+  | ActionSetBossBlind;
 // Utility actions
-export type ActionKeepAlive = { action: 'keepAlive' }
-export type ActionKeepAliveAck = { action: 'keepAliveAck' }
+export type ActionKeepAlive = { action: "keepAlive" };
+export type ActionKeepAliveAck = { action: "keepAliveAck" };
 
-export type ActionUtility = ActionKeepAlive | ActionKeepAliveAck
+export type ActionUtility = ActionKeepAlive | ActionKeepAliveAck;
 
-export type Action = ActionServerToClient | ActionClientToServer | ActionUtility
+export type Action =
+  | ActionServerToClient
+  | ActionClientToServer
+  | ActionUtility;
 
-type HandledActions = ActionClientToServer | ActionUtility
+type HandledActions = ActionClientToServer | ActionUtility;
 export type ActionHandlers = {
-	[K in HandledActions['action']]: keyof ActionHandlerArgs<
-		Extract<HandledActions, { action: K }>
-	> extends never
-	? (
-		// biome-ignore lint/suspicious/noExplicitAny: Function can receive any arguments
-		...args: any[]
-	) => void
-	: (
-		action: ActionHandlerArgs<Extract<HandledActions, { action: K }>>,
-		// biome-ignore lint/suspicious/noExplicitAny: Function can receive any arguments
-		...args: any[]
-	) => void
-}
+  [K in HandledActions["action"]]: keyof ActionHandlerArgs<
+    Extract<HandledActions, { action: K }>
+  > extends never
+    ? (
+        // biome-ignore lint/suspicious/noExplicitAny: Function can receive any arguments
+        ...args: any[]
+      ) => void
+    : (
+        action: ActionHandlerArgs<Extract<HandledActions, { action: K }>>,
+        // biome-ignore lint/suspicious/noExplicitAny: Function can receive any arguments
+        ...args: any[]
+      ) => void;
+};
 
-export type ActionHandlerArgs<T extends HandledActions> = Omit<T, 'action'>
+export type ActionHandlerArgs<T extends HandledActions> = Omit<T, "action">;
 
 // Other types
-export type GameMode = 'attrition' | 'showdown' | 'survival'
+export type GameMode = "attrition" | "showdown" | "survival" | "coopSurvival";
